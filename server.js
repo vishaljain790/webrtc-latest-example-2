5220fa280eac831a34e4a72eee9a2ffa3d0c5ebd
Vishal_team <Vishal.j@myteam11.com>
2018-08-10 12:52:51 +0530

    commmit

--- server.js
+++ server.js
@@ -56,6 +56,6 @@ var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
 });
 
 //listen on port 8080
-webServer.listen(port, function () {
-    console.log('listening on http://localhost:8080');
-});
+// webServer.listen(port, function () {
+//     console.log('listening on http://localhost:8080');
+// });
